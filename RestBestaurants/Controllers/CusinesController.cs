12bbb27dd586e766f34df5bee06f17371a5675e1
Sean Downs <spdowns@protonmail.com>
2020-07-30 11:52:41 -0700

    add more methods in CuisinesControllers and add files in views
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- RestBestaurants/Controllers/CusinesController.cs
+++ RestBestaurants/Controllers/CusinesController.cs
@@ -23,5 +23,23 @@ namespace RestBestaurants.Controllers
     {
       return View();
     }
+    [HttpPost]
+    public ActionResult Create(Cuisine cuisine)
+    {
+      _db.Categories.Add(cuisine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Cuisine thisCuisine = _db.Cuisines.FirstOrDefault(cuisines => cuisines.CuisinesId == id);
+      return View(thisCuisine);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisCuisine = _db.Cuisines.FirstOrDefault(cuisines => cuisines.CuisinesId == id);
+      return View(thisCuisine);
+    }
   }
 }
