31ce809e86483dfb1f700f0143cc0cadbd58f98e
Sean Downs <spdowns@protonmail.com>
2020-07-30 10:52:16 -0700

    updates namespace in cuisines and adds get set in restaurants
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- RestBestaurants/Controllers/CusinesController.cs
+++ RestBestaurants/Controllers/CusinesController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
-namespace RestBestaurants.Controller
+namespace RestBestaurants.Controllers
 {
   public class CuisinesController : Controller
   {
@@ -14,7 +14,6 @@ namespace RestBestaurants.Controller
     {
       _db = db;
     }
-
     
   }
 }
---
+++ RestBestaurants/Models/Restaurant.cs
@@ -0,0 +1,9 @@
+namespace RestBestaurants.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+  }
+}
